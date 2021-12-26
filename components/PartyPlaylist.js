b782bd7069f572f6c4c0cf7196405af98f9e8bae
Miho19 <miho19@hotmail.co.nz>
2021-12-27 10:19:11 +1300

    PartyPlaylist component added

--- components/PartyPlaylist.js
+++ components/PartyPlaylist.js
@@ -5,7 +5,7 @@ export default function PartyPlaylist() {
     <div className="h-full w-full flex flex-col">
       <header className="flex w-full h-10 items-center justify-start pt-5 pb-5">
         <h2 className=" text-white ml-5 font-medium text-xs sm:text-lg lg:text-sm lgg:text-lg">
-          Playlist Name
+          Hello
         </h2>
       </header>
       <main className="w-full h-[calc(100%-2.5rem)] overflow-scroll scrollbar-hide space-y-1">
