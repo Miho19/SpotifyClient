3fda306f5a9c3aa5a8daf80acc9ede0bb935a440
miho19 <miho19@hotmail.co.nz>
2022-01-12 17:10:38 +1300

    more logs

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,9 +2,19 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
+  });
 
   const { pathname } = req.nextUrl;
+
+  console.log("middle ware ", token);
+
+  console.log("middle ware ", pathname);
 
   if (token && pathname === "/login") {
     return NextResponse.redirect("/");
