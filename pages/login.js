6d046f5b03957fdf22b5ae92d4e395e9d4dac9db
miho19 <miho19@hotmail.co.nz>
2022-03-15 16:08:47 +1300

    restyled login page to reflect guest login

--- pages/login.js
+++ pages/login.js
@@ -2,23 +2,29 @@ import React from "react";
 import { getProviders, signIn } from "next-auth/react";
 
 export default function Login({ providers }) {
+  const spotify = providers.spotify;
+
   return (
-    <div className="flex flex-col min-h-screen w-full items-center justify-center bg-black">
+    <div className="flex flex-col min-h-screen w-full items-center justify-center bg-[#161616]">
       <img
         className="w-20 mb-5"
         src="https://links.papareact.com/9xl"
         alt="spotify icon"
       />
-      {Object?.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            className="bg-[#18D860] text-white p-5 rounded-full"
-            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-          >
-            Login with {provider.name}
-          </button>
-        </div>
-      ))}
+      <div className="w-full  h-12 flex items-center justify-center space-x-7">
+        <button
+          onClick={() => signIn(spotify.id, { callbackUrl: "/" })}
+          className="border border-black h-full  px-5 rounded-3xl bg-[#18D860] text-[#161616] font-normal text-lg hover:scale-110 transition-all hover:text-black hover:text-xl hover:font-semibold"
+        >
+          Login
+        </button>
+        <button
+          onClick={() => console.log("guest login")}
+          className="border border-white h-full bg-white px-5 rounded-3xl hover:scale-110 transition-all hover:text-black hover:text-xl hover:font-semibold"
+        >
+          Guest
+        </button>
+      </div>
     </div>
   );
 }
